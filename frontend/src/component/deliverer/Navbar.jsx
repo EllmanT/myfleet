cf843c6b967aa47e3be0795fa2a86e876da0616e
EllmanT <tmuranda1@gmail.com>
2023-08-26 22:31:47 +0200

    updated navbar and sidebar

--- frontend/src/component/deliverer/Navbar.jsx
+++ frontend/src/component/deliverer/Navbar.jsx
@@ -1,114 +1,132 @@
 import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  InputBase,
-  Menu,
-  Menu as MenuIcon,
-  MenuItem,
-  Toolbar,
-  Typography,
-  useTheme,
+    AppBar,
+    Box,
+    Button,
+    IconButton,
+    InputBase,
+    Menu,
+    MenuItem,
+    Toolbar,
+    Typography,
+    useTheme,
 } from "@mui/material";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import FlexBetween from "./FlexBetween";
 import {
-    ArrowDropDownCircleOutlined,
-  DarkModeOutlined,
-  LightModeOutlined,
-  Search,
-  SettingsOutlined,
+    ArrowDropDownOutlined,
+    DarkModeOutlined,
+    LightModeOutlined,
+    Menu as MenuIcon,
+    Search,
+    SettingsOutlined,
 } from "@mui/icons-material";
-import { useDispatch } from "react-redux";
 import { setMode } from "state";
 
 const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
-  const dispatch = useDispatch();
-  const [anchorEl, setAnchorEl] = useState(null);
-  const isOpen = Boolean(anchorEl);
-  const handleClick = (event) => setAnchorEl(event.currentTarget);
-  const handleClose = () => setAnchorEl(null);
-  const theme = useTheme();
-  return (
-    <AppBar
-      sx={{
-        position: "static",
-        background: "none",
-        boxShadow: "5px 5px 10px #ccc ",
-      }}
-    >
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Box alignItems={"center"} display={"flex"} borderRadius={"9px"}>
-          <FlexBetween display={"flex"} color={theme.palette.secondary[200]}>
-            <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-              <MenuIcon />
-            </IconButton>
-            <FlexBetween>
-              <InputBase placeholder="Search" />
-              <IconButton>
-                <Search />
-              </IconButton>
-            </FlexBetween>
-          </FlexBetween>
+    const dispatch = useDispatch();
+    const theme = useTheme();
+    const [anchorEl, setAchorEl] = useState(null);
+    const isOpen = Boolean(anchorEl);
+    const handleClick = (event) => setAchorEl(event.currentTarget);
+    const handleClose = () => setAchorEl(null);
+    return (
+        <AppBar
+            sx={{
+                position: "static",
+                background: "none",
+                boxShadow: "3px 3px 5px #ccc",
+            }}
+        >
+            <Toolbar sx={{ justifyContent: "space-between" }}>
+                {/**Left Side */}
 
-          <FlexBetween gap={"1.5rem"}>
-            <IconButton onClick={()=>dispatch(setMode())}>
-              {theme.palette.mode === "dark" ? (
-                <DarkModeOutlined sx={{ fontSize: "25px" }} />
-              ) : (
-                <LightModeOutlined sx={{ fontSize: "25px" }} />
-              )}
-            </IconButton>
-            <IconButton>
-              <SettingsOutlined sx={{ fontSize: "25px" }} />
-            </IconButton>
+                <FlexBetween color={theme.palette.secondary[200]} gap="1.5rem">
+                    <FlexBetween
+                        backgroundColor={theme.palette.secondary[700]}
+                        borderRadius="9px"
+                        p="0.1rem .5rem"
+                    >
+                        <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                            <MenuIcon />
+                        </IconButton>
+                    </FlexBetween>
+                    <FlexBetween
+                        backgroundColor={theme.palette.secondary[900]}
+                        borderRadius="12px"
+                        p="0.1rem 1rem"
+                    >
+                        <InputBase placeholder="Search" />
+                        <IconButton>
+                            <Search />
+                        </IconButton>
+                    </FlexBetween>
+                </FlexBetween>
 
-            <Button
-            onClick={handleClick}
-           sx={{
-            justifyContent:"space-between",
-            alignItems:"center",
+                {/**Right Side */}
+                <FlexBetween gap={"1.5rem"}>
+                    <IconButton onClick={() => dispatch(setMode())}>
+                        {theme.palette.mode === "dark" ? (
+                            <DarkModeOutlined sx={{ fontSize: "25px" }} />
+                        ) : (
+                            <LightModeOutlined sx={{ fontSize: "25px" }} />
+                        )}
+                    </IconButton>
+                    <IconButton>
+                        <SettingsOutlined sx={{ fontSize: "25px" }} />
+                    </IconButton>
+                    <Button
+                        onClick={handleClick}
+                        sx={{
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            gap: "0.5rem",
+                            textTransform: "none",
+                            display: "flex"
+                        }}
+                    >
+                        <Box
+                            component="img"
+                            alt="profile-img"
+                            height={"32px"}
+                            width={"32px"}
+                            borderRadius={"50%"}
+                            sx={{ objectFit: "cover" }}
+                        />
+                        <Box>
+                            <Typography
+                                fontWeight={"bold"}
+                                fontSize={"0.9rem"}
+                                sx={{ color: theme.palette.secondary[100] }}
+                            >
+                                Tapiwa Muranda
+                            </Typography>
+                            <Typography
+                                fontSize={"0.8rem"}
+                                sx={{ color: theme.palette.secondary[200] }}
+                            >
+                                Admin
+                            </Typography>
+                        </Box>
+                        <ArrowDropDownOutlined
+                            sx={{ color: theme.palette.secondary[200], fontSize: "25px" }}
+                        />
 
-           }}
-            >
-              <Box />
-              <Box display={"flex"} gap ="1.5rem">
-                <Typography
-                  fontWeight={"bold"}
-                  fontSize={"0.9rem"}
-                  sx={{ color: theme.palette.secondary[100] }}
-                >
-                  Tapiwa Muranda
-                </Typography>
-                <Typography
-                  fontSize={"0.8rem"}
-                  sx={{ color: theme.palette.secondary[200] }}
-                >
-                  Admin
-                </Typography>
-              </Box>
-              <ArrowDropDownCircleOutlined 
-               sx={{ color: theme.palette.secondary[200], fontSize:"25px" }}
-               
+                    </Button>
+                    <Menu
+                        open={isOpen}
+                        onClose={handleClose}
+                        anchorEl={anchorEl}
+                        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                    >
+                        <MenuItem onClick={handleClick}>Logout</MenuItem>
+                    </Menu>
 
-              />
+                </FlexBetween>
 
-
-            </Button>
-          </FlexBetween>
-          <Menu 
-          anchorEl={anchorEl}
-          open={isOpen}
-          onClose={handleClose}
-          anchorPosition={{vertical: "center", horizontal:"bottom"}}
-          >
-            <MenuItem>Logout</MenuItem>
-          </Menu>
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
+            </Toolbar>
+        </AppBar>
+    );
 };
 
 export default Navbar;
--- frontend/src/component/deliverer/Sidebar.jsx
+++ frontend/src/component/deliverer/Sidebar.jsx
@@ -11,16 +11,15 @@ import {
   Typography,
   useTheme,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
 import {
   CalendarMonth,
-  ChevronLeft,
   ChevronLeftOutlined,
   HomeOutlined,
-  SettingsOutlined,
+  Money,
 } from "@mui/icons-material";
-import FlexBetween from "./FlexBetween";
 
 const navItems = [
   {
@@ -31,20 +30,27 @@ const navItems = [
     text: "Orders",
     icon: <CalendarMonth />,
   },
+  {
+    text: "Payments",
+    icon: <Money />,
+  },
 ];
 
 const Sidebar = ({
   user,
+  drawerWidth,
   isSidebarOpen,
   setIsSidebarOpen,
-  drawerWidth,
   isNonMobile,
 }) => {
   const { pathname } = useLocation();
-  const [active, setActive] = useState("");
+  const [active, setActive] = useState();
   const theme = useTheme();
   const navigate = useNavigate();
 
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
   return (
     <div component="nav">
       {isSidebarOpen && (
@@ -55,27 +61,26 @@ const Sidebar = ({
           variant="persistent"
           sx={{
             width: drawerWidth,
-            "& .MuiDrawerPaper": {
-              color: theme.palette.secondary[100],
+            "& .MuiDrawer-paper": {
+              color: theme.palette.secondary[300],
               backgroundColor: theme.palette.background.alt,
               boxSizing: "border-box",
-              borderWidth: isNonMobile ? 0 : "2px",
+              borderWidth: isNonMobile ? 0 : "1.5px",
+              width: drawerWidth,
             },
           }}
         >
-          <Box width="100%">
-            <Box m="1.5rem 2rem 2rem 3rem">
-              <FlexBetween
-                alignItems="center"
-                display={"flex"}
-                color={theme.palette.secondary[200]}
-              >
-                <Box gap={"1.5rem"}>
-                  <Typography fontWeight="bold" variant="h4">
+          {/**Part 1 */}
+          <Box width={"100%"}>
+            <Box m="1rem 2rem 2rem 3rem">
+              <FlexBetween color={theme.palette.secondary.main}>
+                <Box alignItems={"center"} gap="1.5rem" display={"flex"}>
+                  <Typography variant="h3" fontWeight={"bold"}>
                     myFleet
                   </Typography>
                 </Box>
-                {isNonMobile && (
+
+                {!isNonMobile && (
                   <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
                     <ChevronLeftOutlined />
                   </IconButton>
@@ -86,7 +91,11 @@ const Sidebar = ({
             <List>
               {navItems.map(({ text, icon }) => {
                 if (!icon) {
-                  return <Typography key={text}>{text}</Typography>;
+                  return (
+                    <Typography key={text} m="1.5rem 2rem 0rem 3rem">
+                      {text}
+                    </Typography>
+                  );
                 }
                 const lcText = text.toLowerCase();
                 return (
@@ -94,7 +103,6 @@ const Sidebar = ({
                     <ListItemButton
                       onClick={() => {
                         navigate(`/del-${lcText}`);
-                        setActive(lcText);
                       }}
                       sx={{
                         backgroundColor:
@@ -113,60 +121,41 @@ const Sidebar = ({
                           color:
                             active === lcText
                               ? theme.palette.primary[600]
-                              : theme.palette.secondary[200],
+                              : theme.palette.secondary[100],
                         }}
                       >
                         {icon}
                       </ListItemIcon>
-                      <ListItemText
-                        primary={text}
-                        sx={{ fontWeight: active === lcText ? "bold" : "" }}
-                      />
+                      <ListItemText primary={text} />
                     </ListItemButton>
                   </ListItem>
                 );
               })}
             </List>
-            <Box position="relative" width="100%">
-              <Divider />
-              <FlexBetween
-                display={"flex"}
-                gap="0.5rem"
-                p="1rem 1rem 0rem 1rem"
-              >
-                <Box
-                  component="img"
-                  alt="profile-img"
-                  height="35px"
-                  width="35px"
-                  borderRadius={"50%"}
-                  sx={{ objectFit: "cover" }}
-                />
-                <Box textAlign={"left"}>
-                  <Typography
-                    fontWeight={"bold"}
-                    fontSize={"0.9rem"}
-                    sx={{ color: theme.palette.secondary[100] }}
-                  >
-                    Tapiwa Muranda
-                  </Typography>
-                  <Typography
-                    fontSize={"0.8rem"}
-                    sx={{ color: theme.palette.secondary[100] }}
-                  >
-                    Admin
-                  </Typography>
-                </Box>
-                <IconButton>
-                  <SettingsOutlined
-                    sx={{
-                      color: theme.palette.secondary[100],
-                      fontSize: "25px",
-                    }}
-                  />
-                </IconButton>
-              </FlexBetween>
+          </Box>
+
+          {/**Part 2 */}
+          <Box width={"100%"} bottom={"0rem"}>
+            <Divider/>
+           <FlexBetween display={"flex"} gap={"1.5rem"} m="1.5rem 2rem 0rem 2rem">
+            <Box component={"img"}
+            alt="profile-img"
+            height={"32px"}
+            width={"32px"}
+            borderRadius={"50%"}
+            sx={{objectFit:"cover"}}
+            />
+            <Box>
+              <Typography fontWeight={"bold"} fontSize={"0.9rem"} sx={{color:theme.palette.secondary[100]}}>
+                Tapiwa Muranda
+              </Typography>
+              <Typography fontSize={"0.8rem"} sx={{color:theme.palette.secondary[200]}}>
+                Admin
+              </Typography>
             </Box>
+           </FlexBetween>
+
+
           </Box>
         </Drawer>
       )}
