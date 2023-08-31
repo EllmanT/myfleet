7d0213dc74cc58d4c92ea1325c8866f0f72cf286
EllmanT <tmuranda1@gmail.com>
2023-08-31 18:06:19 +0200

    Added new routes

--- frontend/src/route/delRoutes.js
+++ frontend/src/route/delRoutes.js
@@ -1,5 +1,27 @@
-import DelRegisterPage from "../pages/deliverer/DelRegisterPage.jsx";
-import DelLoginPage from "../pages/deliverer/DelLoginPage.jsx";
-import DelDashboardPage from "../pages/deliverer/DelDashboardPage.jsx";
+import DelRegisterPage from "../pages/deliverer/RegisterPage.jsx";
+import DelLoginPage from "../pages/deliverer/LoginPage.jsx";
+import DelDashboardPage from "../pages/deliverer/DashboardPage.jsx";
+import DelAdminsPage from "../pages/deliverer/AdminsPage.jsx";
+import DelVehiclesPage from "../pages/deliverer/VehiclesPage.jsx";
+import DelDriversPage from "../pages/deliverer/DriversPage.jsx";
+import DelCustomersPage from "../pages/deliverer/CustomersPage.jsx";
+import DelContractorsPage from "../pages/deliverer/ContractorsPage.jsx";
+import DelPaymentsPage from "../pages/deliverer/PaymentsPage.jsx";
+import DelDashOrdersPage from "../pages/deliverer/DashOrdersPage.jsx";
+import DelDashRevenuePage from "../pages/deliverer/DashRevenuePage.jsx";
+import DelAllOrdersPage from "../pages/deliverer/AllOrdersPage.jsx";
 
-export { DelRegisterPage, DelLoginPage , DelDashboardPage};
+export {
+  DelRegisterPage,
+  DelLoginPage,
+  DelDashboardPage,
+  DelAdminsPage,
+  DelVehiclesPage,
+  DelDriversPage,
+  DelCustomersPage,
+  DelContractorsPage,
+  DelPaymentsPage,
+  DelDashOrdersPage,
+  DelDashRevenuePage,
+  DelAllOrdersPage,
+};
