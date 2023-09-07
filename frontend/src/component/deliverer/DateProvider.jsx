63a1c6613923dbc1d9c259b65ac555b8f90e6407
EllmanT <tmuranda1@gmail.com>
2023-09-07 10:52:16 +0200

    Updated dateProvider

--- frontend/src/component/deliverer/DateProvider.jsx
+++ frontend/src/component/deliverer/DateProvider.jsx
@@ -3,15 +3,14 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-export default function DateProvider({ title, date }) {
-
-
+export default function DateProvider({ title, date, onChange, disabled }) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
         label={title}
-        value={date ? date.toISOString().slice(0, 10) : ""}
-        
+        value={date}
+        onChange={onChange}
+        disabled={disabled}
       />
     </LocalizationProvider>
   );
