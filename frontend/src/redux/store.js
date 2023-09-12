173f7559f4713143a375efe3b409070c26a875de
EllmanT <tmuranda1@gmail.com>
2023-09-12 20:47:12 +0200

    Added the new reducers

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import globalReducer from "../state/index";
 import { userReducer } from "./reducers/user";
+import { customerReducer } from "./reducers/customer";
+import { contractorReducer } from "./reducers/contractor";
+import { vehicleReducer } from "./reducers/vehicle";
 
 const Store = configureStore({
   reducer: {
     global: globalReducer,
     user: userReducer,
+    customer: customerReducer,
+    contractor:contractorReducer,
+    vehicle:vehicleReducer,
   },
 });
 
