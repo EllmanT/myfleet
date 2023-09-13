e25269028b15a085ccc279e4e20b0d3e79179b20
EllmanT <tmuranda1@gmail.com>
2023-09-13 09:50:36 +0200

    Updated design

--- frontend/src/pages/deliverer/DashboardPage.jsx
+++ frontend/src/pages/deliverer/DashboardPage.jsx
@@ -1,9 +1,290 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  TextField,
+  useTheme,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { Add, Close, GroupAdd, SecurityOutlined } from "@mui/icons-material";
+import FlexBetween from "component/deliverer/FlexBetween";
+import Header from "component/deliverer/Header";
+import Cities from "component/Cities";
+import Roles from "component/Roles";
+import { Link } from "react-router-dom";
 
 const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
-}
+  const theme = useTheme();
 
-export default DashboardPage
+  const [open, setOpen] = useState("");
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [role, setRole] = useState("");
+  const [address, setAddress] = useState("");
+  const [companyId] = useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason !== "backdropClick") {
+      setOpen(false);
+    }
+  };
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params) => {
+        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
+      },
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <FlexBetween>
+        <Header title="Dashboard"  />
+        <Box>
+          <Button
+          disabled
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "16px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+          </Button>
+        </Box>
+        <Box>
+          <Link to="/del-register">
+          <Button
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+              ":hover":{
+                backgroundColor: theme.palette.secondary[100],
+
+              }
+            }}
+            onClick={handleClickOpen}
+          >
+            <Add sx={{ mr: "10px" }} />
+            Add Deliverer
+          </Button>
+          </Link>
+        
+        </Box>
+      </FlexBetween>
+
+      <div>
+        <Dialog disableEscapeKeyDown open={open} onClose={handleClose}>
+          <DialogTitle variant="h3" sx={{ m: "0rem 6rem" }}>
+            <Button
+              disabled
+              variant="outlined"
+              sx={{
+                fontSize: "20px",
+                fontWeight: "bold",
+                padding: "10px 20px",
+                ":disabled": {
+                  color: theme.palette.primary[100],
+                },
+              }}
+            >
+              <GroupAdd sx={{ mr: "10px", fontSize: "25px" }} />
+              Admin
+            </Button>
+            <Button
+              variant="outlined"
+              color="info"
+              sx={{ ml: "30px" }}
+              onClick={handleClose}
+            >
+              <Close sx={{ fontSize: "25px" }} />
+            </Button>
+          </DialogTitle>
+          <DialogContent>
+            <form>
+              <Box
+                sx={{ mt: "0.5rem" }}
+                display="flex"
+                maxWidth={"400px"}
+                margin={"auto"}
+                flexDirection="column"
+                alignItems={"center"}
+                justifyContent={"center"}
+              >
+                <Box display={"flex"} flexDirection={"column"}>
+                  <FormControl sx={{ m: 1, minWidth: 250 }}>
+                    <TextField
+                      required
+                      variant="outlined"
+                      type="text"
+                      label="Name"
+                      color="info"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </FormControl>
+                  <FormControl sx={{ m: 1, minWidth: 250 }}>
+                    <TextField
+                      required
+                      variant="outlined"
+                      type="text"
+                      label="Email Address"
+                      color="info"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </FormControl>
+                  <FormControl sx={{ m: 1, minWidth: 250 }}>
+                    <TextField
+                      required
+                      variant="outlined"
+                      type="text"
+                      label="Company Id"
+                      color="info"
+                      value={companyId}
+                      disabled
+                    />
+                  </FormControl>
+                  <Box display={"flex"}>
+                    <FormControl sx={{ m: 1, minWidth: 150 }}>
+                      <Roles
+                        name={role}
+                        onChange={(e) => setRole(e.target.value)}
+                      />
+                    </FormControl>
+                    <FormControl sx={{ m: 1, minWidth: 100 }}>
+                      <TextField
+                        required
+                        variant="outlined"
+                        type="text"
+                        label="Phone Number"
+                        color="info"
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                      />
+                    </FormControl>
+                  </Box>
+                  <Box display={"flex"}>
+                    <FormControl sx={{ m: 1, minWidth: 150 }}>
+                      <Cities
+                        name={city}
+                        onChange={(e) => setCity(e.target.value)}
+                      />
+                    </FormControl>
+                    <FormControl sx={{ m: 1, minWidth: 100 }}>
+                      <TextField
+                        required
+                        variant="outlined"
+                        type="text"
+                        label="Home Address"
+                        color="info"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                      />
+                    </FormControl>
+                  </Box>
+
+                  <Box display={"flex"} sx={{ m: "1rem 3rem " }}>
+                    <Button
+                      onClick={handleClose}
+                      variant="contained"
+                      size="large"
+                      sx={{
+                        color: theme.palette.secondary[300],
+
+                        margin: "1rem",
+                        border: "solid 1px",
+                        ":hover": {
+                          backgroundColor: theme.palette.secondary[800],
+                        },
+                        ":disabled": {
+                          backgroundColor: theme.palette.secondary[800],
+                        },
+                      }}
+                    >
+                      Close
+                    </Button>
+                    <Button
+                      variant="contained"
+                      fontWeight="bold"
+                      sx={{
+                        color: theme.palette.secondary[100],
+                        backgroundColor: theme.palette.secondary[300],
+                        margin: "1rem  ",
+                        border: "solid 0.5px",
+                        ":hover": {
+                          backgroundColor: theme.palette.secondary[300],
+                        },
+                        ":disabled": {
+                          backgroundColor: theme.palette.secondary[300],
+                        },
+                      }}
+                    >
+                      Add Admin
+                    </Button>
+                  </Box>
+                </Box>
+              </Box>
+            </form>
+          </DialogContent>
+          <DialogActions></DialogActions>
+        </Dialog>
+      </div>
+
+      {/**Content starts here */}
+
+      {/**Content ends here */}
+    </Box>
+  );
+};
+
+export default DashboardPage;
