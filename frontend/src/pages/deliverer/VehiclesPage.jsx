c36a1a3de48033b0b065ab040d4915f9c9abd29a
EllmanT <tmuranda1@gmail.com>
2023-09-13 09:49:50 +0200

    Updated design

--- frontend/src/pages/deliverer/VehiclesPage.jsx
+++ frontend/src/pages/deliverer/VehiclesPage.jsx
@@ -19,98 +19,65 @@ import {
   useTheme,
 } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { Add, Close, Group, GroupAdd, LocalShipping, PhotoSizeSelectActual } from "@mui/icons-material";
+import {
+  Add,
+  Close,
+  Group,
+  GroupAdd,
+  LocalShipping,
+  PhotoSizeSelectActual,
+} from "@mui/icons-material";
 import GoodsTypes from "component/deliverer/GoodsType";
 import FlexBetween from "component/deliverer/FlexBetween";
 import Header from "component/deliverer/Header";
 import DateProvider from "component/deliverer/DateProvider";
+import { useDispatch } from "react-redux";
+import { createVehicle } from "redux/actions/vehicle";
 
-
-const sizes= [
+const sizes = [
   {
-    value:"Small (<=5T)",
+    value: "Small (<=5T)",
   },
   {
-    value:"Medium (<=10T)",
+    value: "Medium (<=10T)",
   },
   {
-    value:"Horse (>=10T)"
-
-
-  }
-]
+    value: "Horse (>=10T)",
+  },
+];
 
 const VehiclesPage = () => {
   const theme = useTheme();
+  const dispatch = useDispatch();
 
   const [open, setOpen] = useState("");
 
-
-
-  const [name, setName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [city, setCity] = useState("");
-  const [description, setDescription] = useState("");
-
-
-  //the steps
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [completed, setCompleted] = React.useState({});
-  //the customer info
-  const [customer, setCustomer] = useState("");
-  const [from, setFrom] = useState("");
-  const [address, setAddress] = useState("");
-  const [extraInfo, setExtraInfo] = useState("");
-  const [contractorId, setContractorId] = useState("");
-  const [orderDate, setOrderDate] = useState(null);
-
-  //the company info
-  const [driverId, setDriverId] = useState("");
-  const [vehicleId, setVehicleId] = useState("");
-  const [mileageOut, setMileageOut] = useState("");
-  const [mileageIn, setMileageIn] = useState("");
-
-  //the preview
-  const distance = mileageIn - mileageOut;
-  const cost = distance * 1.65;
-
-//start of add Contractor
-  //for the goods type add contractor
-  const [goodsType, setGoodsType] = useState([]);
-
-
-  //end of add contractor
-
-  //start of add Order
-
-  //end of add Order
-
-  //start of add driver
-
-  //end of add drover
-
-
-
-
-
-
-
-
-  //the steps
+  const [make, setMake] = useState("");
+  const [size, setSize] = useState("");
+  const [regNumber, setRegNumber] = useState("");
+  const [companyId, setCompanyId] = useState("");
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-
-
-
   const handleClose = (event, reason) => {
     if (reason !== "backdropClick") {
       setOpen(false);
-    
-
     }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newForm = new FormData();
+
+    newForm.append("make", make);
+    newForm.append("regNumber", regNumber);
+    newForm.append("size", size);
+    newForm.append("companyId", companyId);
+
+    dispatch(createVehicle(newForm));
   };
 
   const columns = [
@@ -156,45 +123,43 @@ const VehiclesPage = () => {
 
   return (
     <Box m="1.5rem 2.5rem">
-    <FlexBetween>
-<Header title="Vehicles" subtitle="See all your vehicles." />
-<Box >
+      <FlexBetween>
+        <Header title="Vehicles" subtitle="See all your vehicles." />
+        <Box>
+          <Button
+            disabled
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "16px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <LocalShipping sx={{ mr: "10px" }} />4
+          </Button>
+        </Box>
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+              ":hover": {
+                backgroundColor: theme.palette.secondary[100],
+              },
+            }}
+            onClick={handleClickOpen}
+          >
+            <Add sx={{ mr: "10px" }} />
+            Add
+          </Button>
+        </Box>
+      </FlexBetween>
 
-  <Button
-    sx={{
-      backgroundColor: theme.palette.secondary.light,
-      color: theme.palette.background.alt,
-      fontSize: "14px",
-      fontWeight: "bold",
-      padding: "10px 20px",
-    }}
-  >
-    <LocalShipping sx={{ mr: "10px" }} />
-    4
-  </Button>
-</Box>
-<Box >
-
-  <Button
-    sx={{
-      backgroundColor: theme.palette.secondary.light,
-      color: theme.palette.background.alt,
-      fontSize: "14px",
-      fontWeight: "bold",
-      padding: "10px 20px",
-      ":hover": {
-        backgroundColor: theme.palette.secondary[100],
-      },
-    }}
-    onClick={handleClickOpen}
-  >
-    <Add sx={{ mr: "10px" }} />
-    Add
-  </Button>
-</Box>
-</FlexBetween>
-
-<div>
+      <div>
         <Dialog disableEscapeKeyDown open={open} onClose={handleClose}>
           <DialogTitle variant="h3" sx={{ m: "0rem 6rem" }}>
             <Button
@@ -212,128 +177,134 @@ const VehiclesPage = () => {
               <GroupAdd sx={{ mr: "10px", fontSize: "25px" }} />
               Vehicle
             </Button>
-            <Button variant="outlined" color="info" sx={{ml: "30px"}} onClick={handleClose}>
-              <Close sx={{fontSize:"25px"}}/>
+            <Button
+              variant="outlined"
+              color="info"
+              sx={{ ml: "30px" }}
+              onClick={handleClose}
+            >
+              <Close sx={{ fontSize: "25px" }} />
             </Button>
           </DialogTitle>
           <DialogContent>
-            <form>
-            <Box
-                        sx={{ mt: "0.5rem" }}
-                        display="flex"
-                        maxWidth={"400px"}
-                        margin={"auto"}
-                        flexDirection="column"
-                        alignItems={"center"}
-                        justifyContent={"center"}
-                      >
-
-<Box display={"flex"} flexDirection={"column"}>
-                <FormControl sx={{ m: 1, minWidth: 250 }}>
-                  <TextField
-                    required
-                    variant="outlined"
-                    type="text"
-                    label="Make"
-                    color="info"
-                  />
-                </FormControl>
-                <Box display={"flex"}>
-                  <FormControl sx={{ m: 1, minWidth: 150 }}>
-                    <InputLabel id="demo-simple-select-autowidth-label">
-                      Size
-                    </InputLabel>
-                    <Select
-                      labelId="simple-select-autowidth-label"
-                      id="demo-simple-select-autowidth"
-                      autoWidth
-                      label="Size"
-                    > 
-                     {sizes.map((option) => (
-                                    <MenuItem
-                                      key={option.value}
-                                      value={option.value}
-                                    >
-                                      {option.value}
-                                    </MenuItem>
-                                  ))}
-
-                    </Select>
-                  </FormControl>
-                  <FormControl sx={{ m: 1, minWidth: 100 }}>
+            <form onSubmit={handleSubmit}>
+              <Box
+                sx={{ mt: "0.5rem" }}
+                display="flex"
+                maxWidth={"400px"}
+                margin={"auto"}
+                flexDirection="column"
+                alignItems={"center"}
+                justifyContent={"center"}
+              >
+                <Box display={"flex"} flexDirection={"column"}>
+                  <FormControl sx={{ m: 1, minWidth: 250 }}>
                     <TextField
                       required
                       variant="outlined"
                       type="text"
-                      label="Reg Number"
+                      label="Make"
                       color="info"
+                      value={make}
+                      onChange={(e) => setMake(e.target.value)}
                     />
                   </FormControl>
-                </Box>
+                  <Box display={"flex"}>
+                    <FormControl sx={{ m: 1, minWidth: 150 }}>
+                      <InputLabel id="demo-simple-select-autowidth-label">
+                        Size
+                      </InputLabel>
+                      <Select
+                        labelId="simple-select-autowidth-label"
+                        id="demo-simple-select-autowidth"
+                        autoWidth
+                        label="Size"
+                        value={size}
+                        onChange={(e) => setSize(e.target.value)}
+                      >
+                        {sizes.map((option) => (
+                          <MenuItem key={option.value} value={option.value}>
+                            {option.value}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                    <FormControl sx={{ m: 1, minWidth: 100 }}>
+                      <TextField
+                        required
+                        variant="outlined"
+                        type="text"
+                        label="Reg Number"
+                        color="info"
+                        value={regNumber}
+                        onChange={(e) => setRegNumber(e.target.value)}
+                      />
+                    </FormControl>
+                  </Box>
 
-                <FormControl sx={{ m: 1, minWidth: 250 }}>
-                  <TextField
-                    required
-                    variant="outlined"
-                    type="text"
-                    label="Address"
-                    color="info"
-                  />
-                </FormControl>
+                  <FormControl sx={{ m: 1, minWidth: 250 }}>
+                    <TextField
+                      disabled
+                      variant="outlined"
+                      type="text"
+                      label="Company Id"
+                      color="info"
+                      value={companyId}
+                    />
+                  </FormControl>
 
-                <Box display={"flex"} sx={{ m: "1rem 3rem " }}>
-                  <Button
-                    onClick={handleClose}
-                    variant="contained"
-                    size="large"
-                    sx={{
-                      color: theme.palette.secondary[300],
+                  <Box display={"flex"} sx={{ m: "1rem 3rem " }}>
+                    <Button
+                      onClick={handleClose}
+                      variant="contained"
+                      size="large"
+                      sx={{
+                        color: theme.palette.secondary[300],
 
-                      margin: "1rem",
-                      border: "solid 1px",
-                      ":hover": {
-                        backgroundColor: theme.palette.secondary[800],
-                      },
-                      ":disabled": {
-                        backgroundColor: theme.palette.secondary[800],
-                      },
-                    }}
-                  >
-                    Close
-                  </Button>
-                  <Button
-                    variant="contained"
-                    fontWeight="bold"
-                    sx={{
-                      color: theme.palette.secondary[100],
-                      backgroundColor: theme.palette.secondary[300],
-                      margin: "1rem  ",
-                      border: "solid 0.5px",
-                      ":hover": {
+                        margin: "1rem",
+                        border: "solid 1px",
+                        ":hover": {
+                          backgroundColor: theme.palette.secondary[800],
+                        },
+                        ":disabled": {
+                          backgroundColor: theme.palette.secondary[800],
+                        },
+                      }}
+                    >
+                      Close
+                    </Button>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      fontWeight="bold"
+                      sx={{
+                        color: theme.palette.secondary[100],
                         backgroundColor: theme.palette.secondary[300],
-                      },
-                      ":disabled": {
-                        backgroundColor: theme.palette.secondary[300],
-                      },
-                    }}
-                  >
-                    Add Vehicle
-                  </Button>
+                        margin: "1rem  ",
+                        border: "solid 0.5px",
+                        ":hover": {
+                          backgroundColor: theme.palette.secondary[300],
+                        },
+                        ":disabled": {
+                          backgroundColor: theme.palette.secondary[300],
+                        },
+                      }}
+                    >
+                      Add Vehicle
+                    </Button>
+                  </Box>
                 </Box>
               </Box>
-                      </Box>
-    
             </form>
           </DialogContent>
           <DialogActions></DialogActions>
         </Dialog>
       </div>
 
-{/**Content starts here */}
+      {/**Content starts here */}
 
-{/**Content ends here */}
-
-</Box>
+      {/**Content ends here */}
+    </Box>
   );
 };
 
