8584c12d09314de5f89ab146f8c950de5e2f0bdc
EllmanT <tmuranda1@gmail.com>
2023-09-18 19:33:10 +0200

    Updated time token expires

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -4,14 +4,14 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
   const options = {
-    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     Secure: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
-    
+
     user,
     token,
   });
