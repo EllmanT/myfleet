96db325971e65fd11cfd07681538b961e321dc91
EllmanT <tmuranda1@gmail.com>
2023-09-20 17:36:39 +0200

    Updated fields

--- backend/model/customer.js
+++ backend/model/customer.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const customerSchema = new mongoose.Schema({
   name: {
     type: String,
+    required:true,
   },
   city: {
     type: String,
+    
   },
   phoneNumber: {
-    type: Number,
+    type: String,
   },
   address: {
     type: String,
