fcd8ba9dc2767071be812c836cb47ed8396d7e0b
EllmanT <tmuranda1@gmail.com>
2023-09-20 17:33:31 +0200

    Added clear message feature

--- frontend/src/redux/reducers/contractor.js
+++ frontend/src/redux/reducers/contractor.js
@@ -1,33 +1,36 @@
 import { createReducer } from "@reduxjs/toolkit";
 
 const initialState = {
-    isLoading : true,
-}
+  isLoading: true,
+};
 
-export const contractorReducer = createReducer(initialState,{
+export const contractorReducer = createReducer(initialState, {
+  loadCreateContractorRequest: (state) => {
+    state.isLoading = true;
+  },
 
-    "loadContractorCreateRequest": (state)=>{
-        state.isLoading = true;
+  //load contractor success
 
-    },
+  loadCreateContractorSuccess: (state, action) => {
+    state.isLoading = false;
+    state.success = true;
+    state.contractor = action.payload;
+  },
 
-    //load contractor success
+  //load contractor failed
+  loadCreateContractorFailed: (state, action) => {
+    state.isLoading = false;
+    state.success = false;
+    state.error = action.payload;
+  },
 
-    "loadContractorCreateSuccess":(state,action)=>{
-        state.isLoading = false;
-        state.success = true;
-        state.contractor = action.payload;
-    },
+  //clear errors
+  clearErrors: (state) => {
+    state.error = null;
+  },
 
-    //load contractor failed
-    "loadContractorCreateFailed":(state,action)=>{
-        state.isLoading  = false;
-        state.success= false;
-        state.error = action.payload;
-    },
-
-    //clear errors
-    "clearErrors":(state)=>{
-        state.error = null
-    }
-})
+  //clear success messages
+  clearMessages: (state) => {
+    state.success = null;
+  },
+});
