1c3c0918b900d4e48ac60e5e8a6c36833d3a3197
EllmanT <tmuranda1@gmail.com>
2023-09-20 17:38:13 +0200

    Finished the create logic

--- backend/controller/driver.js
+++ backend/controller/driver.js
@@ -4,39 +4,62 @@ const Driver = require("../model/driver");
 const { upload } = require("../multer");
 const ErrorHandler = require("../utils/ErrorHandler");
 const router = express.Router();
-const fs = require("fs")
+const fs = require("fs");
 
-router.post("/create-driver", upload.single("file"), async (req, res, next) => {
-  try {
-    const { name, phoneNumber, address, city, idNumber } = req.body;
+router.post(
+  "/create-driver",
+  upload.fields([
+    { name: "license", maxCount: 1 },
+    { name: "id", maxCount: 1 },
+  ]),
+  async (req, res, next) => {
+    try {
+      const { name, phoneNumber, address, city, idNumber, companyId } =
+        req.body;
 
-    let Driver = await Driver.findOne({ idNumber });
-    if (Driver) {
-        const filename = req.file.filename;
-        const filePath = `uploads/${filename}`;
-        fs.unlink(filePath, (err) => {
+      let addDriver = await Driver.findOne({ idNumber });
+
+      if (addDriver) {
+        const license = req.files.license;
+        const licensePath = `uploads/${license}`;
+        fs.unlink(licensePath, (err) => {
           if (err) {
             console.log(err);
             res.status(500).json({ message: "Error deleting file" });
           }
         });
-      return next(new ErrorHandler("Driver exists", 400));
+        const id = req.files.id;
+        const idPath = `uploads/${id}`;
+        fs.unlink(idPath, (err) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({ message: "Error deleting file" });
+          }
+        });
+        return next(new ErrorHandler("Driver exists", 400));
+      }
+      const license = req.files.license[0];
+      const licenseUrl = path.join(license.path);
+      const id = req.files.id[0];
+      const idUrl = path.join(id.path);
+      addDriver = await Driver.create({
+        name: name,
+        phoneNumber: phoneNumber,
+        address: address,
+        city: city,
+        idNumber: idNumber,
+        companyId: companyId,
+        license: licenseUrl,
+        id: idUrl,
+      });
+      res.status(201).json({
+        success: true,
+        Driver,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 400));
     }
-    const filename = req.file.filename;
-    const fileUrl = path.join(filename);
-    checkDriver = await Driver.create({
-      name: name,
-      phoneNumber: phoneNumber,
-      address: address,
-      city: city,
-      idNumber: idNumber,
-      licence: fileUrl,
-    });
-    res.status(201).json({
-      success: true,
-      Driver,
-    });
-  } catch (error) {
-    return next(new ErrorHandler(error.message, 400));
   }
-});
+);
+
+module.exports = router;
