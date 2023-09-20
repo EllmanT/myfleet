bf03dda30cb4a07c2380f78f878ad36ce952220f
EllmanT <tmuranda1@gmail.com>
2023-09-20 17:34:03 +0200

    fixed upload error

--- backend/multer.js
+++ backend/multer.js
@@ -1,13 +1,16 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, res, next) {
-    cb(null, "uploads/");
+  destination: function (req, res,cb) {
+   cb(null, "uploads/");
   },
-  filename: function (cb, file, res) {
-    const uniqueSuffix = Date.now + "-" + Math.round(Math.random() * 1e9);
+  filename: function (req, file,cb ) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const filename = file.originalname.split(".")[0];
     cb(null, filename + "-" + uniqueSuffix + ".png");
   },
 });
+
 exports.upload = multer({ storage: storage });
+
+
