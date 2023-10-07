c430a951c71cd9328dfaf11641d9f9aa6e8a071b
EllmanT <tmuranda1@gmail.com>
2023-10-07 12:44:56 +0200

    Get all vehicles for deliverer

--- frontend/src/redux/actions/customer.js
+++ frontend/src/redux/actions/customer.js
@@ -7,7 +7,7 @@ export const createCustomer = (newForm) => async (dispatch) => {
       type: "loadCustomerCreateRequest",
     });
 
-    const  config  = { Headers: { "Content-Type": "multipart/formdata" } };
+    const config = { Headers: { "Content-Type": "multipart/formdata" } };
 
     const { data } = await axios.post(
       `${server}/customer/create-customer`,
@@ -21,6 +21,29 @@ export const createCustomer = (newForm) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: "loadCustomerCreateFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// get All Customers for a deliverer
+export const getAllCustomersDeliverer = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "getAllCustomersDelivererRequest",
+    });
+
+    const { data } = await axios.get(
+      `${server}/customer/get-all-customers-company`,
+      { withCredentials: true }
+    );
+    dispatch({
+      type: "getAllCustomersDelivererSuccess",
+      payload: data.customers,
+    });
+  } catch (error) {
+    dispatch({
+      type: "getAllCustomersDelivererFailed",
       payload: error.response.data.message,
     });
   }
